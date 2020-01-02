32b515ea654745acbf352c218e764bed2ef24b03
claridiva2000 <claridiva2000@gmail.com>
2020-01-02 16:28:03 -0600

    psuedocoding components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import Header from '../Misc/Header'
+import SocialMediaFooter from "../Misc/SocialMediaFooter";
 
 const Login = () => {
-  return (
-    <div>
-      Login Component
-    </div>
-  )
-}
+  const handleChange = e => {};
 
-export default Login
+  const handleSubmit = e => {};
+
+  return (
+    //nav
+    <div className="login-container">
+      <nav>
+        <button className="nav-btn">Docs</button>
+      </nav>
+      <nav>
+        <button className="nav-btn">About</button>
+        <button className="nav-btn">Create Account</button>
+      </nav>
+      <Header/>
+      <form action="" className="login-form">
+        <input type="text" className="login-input" />
+        <input type="text" className="login-input" />
+        <button className="login-btn">Login</button>
+      </form>
+      <SocialMediaFooter />
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Misc/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <div className="title">
+        <div className="logo">Logo</div>
+        <h1>WriteInspyre</h1>
+      </div>
+  )
+}
+
+export default Header
